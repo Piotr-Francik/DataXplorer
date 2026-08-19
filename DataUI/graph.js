cda49c9e2844cf5fc35113f672dbfb8775039aef
JacobConnor <gn24034@bristol.ac.uk>
2026-08-19 02:57:30 +0100

    plotted some ctd graph with some dummy data

--- DataUI/graph.js
+++ DataUI/graph.js
@@ -1,24 +1,56 @@
-const xValues = [100,200,300,400,500,600,700,800,900,1000];
 
-new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-      borderColor: "red",
-      fill: false
-    },{
-      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-      borderColor: "green",
-      fill: false
-    },{
-      data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-      borderColor: "blue",
-      fill: false
-    }]
-  },
-  options: {
-    legend: {display: false}
+function parsing(){
+  console.log("before running")
+  return fetch("./1974Data.csv")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Could not load CSV: ${response.status} ${response.statusText}`);
+      }
+      return response.text();
+    })
+    .then(csvText =>{
+      const rows = csvText.split("\n")
+      for(let i = 0; i < rows.length; i++){
+        rows[i] = rows[i].split(",")
+      }
+      console.log(rows)
+      return rows
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  
+  console.log("after running")
+}
+
+parsing().then(data => {
+  const xValues = []
+  const tempurature = []
+  const salinity = []
+
+  for(let i = 0; i < data.length; i++){
+    xValues[i] = data[i][1]
+    tempurature[i] = data[i][2]
+    salinity[i] = data[i][3]
   }
+
+  new Chart("myChart", {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [{
+        data: tempurature,
+        borderColor: "red",
+        fill: false
+      },{
+        data: salinity,
+        borderColor: "green",
+        fill: false
+      }]
+    },
+    options: {
+      legend: {display: false}
+    }
+  });
 });
+
